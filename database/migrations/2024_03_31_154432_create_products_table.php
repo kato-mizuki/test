137d9d49d8f29be69b7b3e065c67a1406ed6d6e4
kato-mizuki <mk1141629@gmail.com>
2024-04-01 00:59:45 +0900

    Second Commit

--- database/migrations/2024_03_31_154432_create_products_table.php
+++ database/migrations/2024_03_31_154432_create_products_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('product_name');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->text('comment')->nullable();
+            $table->string('img_path');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('products');
     }
 };
